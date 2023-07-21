3b7fc76b0bb4e5431f4d06cdcf011027dc39dfd8
Gabriel <matiascoria427@gmail.com>
2023-07-21 12:05:26 -0300

    se agregaron todos los test del controller de tarjeta y se estan agregando los test del controller de usuario

--- UnitTest/TarjetaControllerTest.cs
+++ UnitTest/TarjetaControllerTest.cs
@@ -18,7 +18,8 @@ using System.Threading.Tasks;
 namespace UnitTest
 {
     public class TarjetaControllerTest
-    {
+    {   
+        //crearTarjeta
 
         [Fact]
         public void TestCreateTarjetaInService()
@@ -34,31 +35,105 @@ namespace UnitTest
             var usuarioId = Guid.NewGuid();
             var tarjetaId = Guid.NewGuid();
 
-            var request = new TarjetaRequest
+            var requestTarjeta = new TarjetaRequest
             {
                 NumeroTarjeta = "4458 4810 1584 1256",
                 TipoTarjeta = "visa",
                 Vencimiento = new DateTime(2029, 10, 1),
+                EntidadTarjeta = "visa",
                 usuarioId = usuarioId
             };
 
-            var tarjetaFalsa = new Tarjeta
+;
+            var usuarioFalso = new UsuarioResponse
             {
-                NumeroTarjeta = request.NumeroTarjeta,
-                EntidadTarjeta = request.EntidadTarjeta,
-                TarjetaId = tarjetaId,
-                UsuarioId = usuarioId
+                Nombre = "Miguel",
+                Apellido = "Correa",
+                Dni = "23869487",
+                usuarioId = usuarioId
             };
 
-            mockCommand.Setup(c => c.InsertTarjeta(It.IsAny<Tarjeta>())).Returns(tarjetaFalsa);
-            mockUsuarioService.Setup(us => us.GetUsuarioById(It.IsAny<Guid>())).Returns(new UsuarioResponse());
+          
+            mockUsuarioService.Setup(us => us.GetUsuarioById(It.IsAny<Guid>())).Returns(usuarioFalso);
+           
 
             // Act
-            var result = controller.CreateTarjeta(request);
+            var result = controller.CreateTarjeta(requestTarjeta);
 
             // Assert
             Assert.NotNull(result);
+
+            var jsonResult = result as JsonResult;
+            var response = jsonResult.Value as TarjetaResponse;
+
+            Assert.Equal(response.NumeroTarjeta, requestTarjeta.NumeroTarjeta);
+            Assert.Equal(response.EntidadTarjeta, requestTarjeta.EntidadTarjeta);
+    
+
+            Assert.Equal(response.Usuario.Nombre, usuarioFalso.Nombre);
+            Assert.Equal(response.Usuario.Apellido, usuarioFalso.Apellido);
+            Assert.Equal(response.Usuario.Dni, usuarioFalso.Dni);
+            Assert.Equal(response.Usuario.usuarioId, usuarioFalso.usuarioId);
         }
+
+
+        [Fact]
+        public void TestCreateTarjetaExcepcion()
+        {
+            // Arrange
+            var mockTarjetaService = new Mock<ITarjetaService>();
+
+            mockTarjetaService.Setup(ts => ts.CreateTarjeta(It.IsAny<TarjetaRequest>())).Throws(new Exception());
+
+            var controller = new TarjetaController(mockTarjetaService.Object);
+            var expectedMessage = "error al registrar la tarjeta";
+            var expectedStatusCode = 404;
+
+            //ACT
+            var result = controller.CreateTarjeta(new TarjetaRequest());
+
+            //ASSERT
+            Assert.IsType<JsonResult>(result);
+            var jsonResult = result as JsonResult;
+
+            Assert.NotNull(jsonResult);
+            Assert.Equal(expectedStatusCode, jsonResult.StatusCode);
+
+            var badRequest = jsonResult.Value as BadRequest;
+            Assert.Equal(expectedMessage, badRequest.message);
+        }
+
+        [Fact]
+        public void TestCreateTarjetaNull()
+        {
+            // Arrange
+            var mockTarjetaService = new Mock<ITarjetaService>();
+
+            mockTarjetaService.Setup(ts => ts.CreateTarjeta(It.IsAny<TarjetaRequest>())).Returns((TarjetaResponse)null);
+
+            var controller = new TarjetaController(mockTarjetaService.Object);
+
+            var expectedMessage = "no se creo la tarjeta";
+
+            //ACT
+            var result = controller.CreateTarjeta(new TarjetaRequest());
+
+            //ASSERT
+            Assert.IsType<JsonResult>(result);
+            var jsonResult = result as JsonResult;
+
+            Assert.NotNull(jsonResult);
+            var badRequest = jsonResult.Value as BadRequest;
+
+            Assert.NotNull(badRequest);
+            Assert.Equal(expectedMessage, badRequest.message);
+        }
+
+
+
+
+
+        //Test metodo GetTarjetasUsuario
 
         [Fact]
         public void TestGetTarjetaUserNulo()
@@ -70,6 +145,7 @@ namespace UnitTest
             var usuarioId = Guid.NewGuid();
             int expectedStatus = 404;
             string expectedMessage = "no hemos encontrado nada";
+
             mockTarjetaService.Setup(ts => ts.GetUsuarioTarjetas(It.IsAny<Guid>())).Returns((TarjetasUsuarioResponse)null);
 
             //ACT
@@ -80,7 +156,7 @@ namespace UnitTest
             
             Assert.IsType<JsonResult>(result);
             var jsonResult = result as JsonResult;
-            var badRequest = jsonResult.Value as BadRequest;
+            var badRequest = jsonResult?.Value as BadRequest;
             Assert.NotNull(badRequest);
             Assert.Equal(expectedStatus, jsonResult.StatusCode);
             Assert.Equal(expectedMessage, badRequest.message);
@@ -111,7 +187,7 @@ namespace UnitTest
 
 
         [Fact]
-        public void TestGetTarjetaUsuario()
+        public void TestGetTarjetaUsuarioOk()
         {
             // ARRANGE
             var mockTarjetaService = new Mock<ITarjetaService>();
@@ -138,8 +214,13 @@ namespace UnitTest
 
             // ASSERT
             Assert.IsType<JsonResult>(result);
+
             var jsonResult = result as JsonResult;
-            Assert.Equal(tarjetasUsuarioResponse, jsonResult.Value);
+            var resultTarjetaResponse = jsonResult?.Value as TarjetasUsuarioResponse;
+
+            Assert.Equal(tarjetasUsuarioResponse.tarjetasUsuario, resultTarjetaResponse?.tarjetasUsuario);
+            Assert.Equal(tarjetasUsuarioResponse.nombre, resultTarjetaResponse.nombre);
+            Assert.Equal(tarjetasUsuarioResponse.usuarioId, resultTarjetaResponse.usuarioId);
       
         }
 
---
+++ UnitTest/UsuarioControllerTest.cs
@@ -0,0 +1,148 @@
+﻿using Application.Interfaces;
+using Application.Request;
+using Application.Response;
+using Application.UseCase.Tokens;
+using Domain.Entities;
+using microservicio.usuario.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    public class UsuarioControllerTest
+    {
+        //test de autenticacion
+
+        [Fact]
+        public void LoginAuthOk()
+        {
+            //ARRANGE
+            string firma = "unajproyectodesoftwareclaveparatokenjwt12334estoessolounaprueba2";
+            var tokenService = new TokenService(firma);
+            var mockUsuarioService = new Mock<IUsuarioService>();
+            var controller = new UsuarioController(mockUsuarioService.Object);
+
+            var usuarioLogin = new UsuariLoginRequest
+            {
+                email = "mariano@hotmail.com",
+                password = "secretPassword123*"
+            };
+
+            var usuario = new Usuario
+            {
+                Email = usuarioLogin.email,
+                Nombre = "Mario",
+                UsuarioId = Guid.NewGuid()
+            };
+
+            var usuarioTokenResponse = tokenService.GenerateToken(usuario);
+            
+
+            mockUsuarioService.Setup(us => us.Authenticacion(It.IsAny<UsuariLoginRequest>())).Returns(usuarioTokenResponse);
+
+            //ACT
+            var result = controller.LoginAuth(usuarioLogin);
+
+            //ASSERT
+
+            Assert.IsType<OkObjectResult>(result);
+            var okResponse = result as OkObjectResult;
+            var authResponse = okResponse.Value as UsuarioTokenResponse;
+
+            Assert.NotNull(authResponse);
+            Assert.Equal(authResponse.Token,usuarioTokenResponse.Token);
+            Assert.Equal(authResponse.UsuarioId, usuario.UsuarioId);
+
+            //decoficar token
+            var handler = new JwtSecurityTokenHandler();
+            var token = handler.ReadJwtToken(authResponse.Token);
+
+            var uniqueNameClaim = token.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.UniqueName);
+            Assert.Equal(usuario.UsuarioId.ToString(), uniqueNameClaim?.Value);
+
+            var nameClaim = token.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Name);
+            Assert.Equal(usuario.Nombre, nameClaim?.Value);
+
+            var emailClaim = token.Claims.FirstOrDefault(c => c.Type == JwtRegisteredClaimNames.Email);
+            Assert.Equal(usuario.Email, emailClaim?.Value);
+
+        }
+
+
+
+        [Fact]
+        public void LoginAuthNulo()
+        {
+            //ARRANGE
+            string firma = "unajproyectodesoftwareclaveparatokenjwt12334estoessolounaprueba2";
+            var tokenService = new TokenService(firma);
+            var mockUsuarioService = new Mock<IUsuarioService>();
+            var controller = new UsuarioController(mockUsuarioService.Object);
+
+            var usuarioLogin = new UsuariLoginRequest
+            {
+                email = "mariano@hotmail.com",
+                password = "secretPassword123*"
+            };
+
+            var expectedMessage = "usuario invalido";
+
+            mockUsuarioService.Setup(us => us.Authenticacion(It.IsAny<UsuariLoginRequest>())).Returns((UsuarioTokenResponse)null);
+
+            //ACT
+            var result = controller.LoginAuth(usuarioLogin);
+
+            //ASSERT
+            Assert.IsType<BadRequestObjectResult>(result);
+
+            var badRequest = result as BadRequestObjectResult;
+            var requestBad = badRequest?.Value as BadRequest;
+
+            Assert.Equal(requestBad?.message, expectedMessage);
+                    
+        }
+
+
+        //test de GetUsuarioById
+        [Fact]
+        public void GetUusarioByIdOk()
+        {
+            //ARRANGE
+            var mockUsuarioService = new Mock<IUsuarioService>();
+            var controller = new UsuarioController(mockUsuarioService.Object);
+
+            var idUsuarioValido = Guid.NewGuid().ToString();
+
+            var usuarioFalsoResponse = new UsuarioResponse
+            {
+                usuarioId = new Guid(idUsuarioValido),
+                Nombre = "Mario",
+                Apellido = "Riquelme",
+                Dni = "34456766"
+            };
+
+            mockUsuarioService.Setup(us => us.GetUsuarioById(It.IsAny<Guid>())).Returns(usuarioFalsoResponse);
+
+            //ACT
+            var result = controller.GetUsuarioById(idUsuarioValido);
+
+            //ASSERT
+            Assert.IsType<JsonResult>(result);
+
+            var jsonResult = result as JsonResult;
+            var response = jsonResult?.Value as UsuarioResponse;
+
+            Assert.Equal(response.Nombre,usuarioFalsoResponse.Nombre);
+            Assert.Equal(response.Apellido,usuarioFalsoResponse.Apellido);
+            Assert.Equal(response.Dni,usuarioFalsoResponse.Dni);
+            Assert.Equal(response.usuarioId,usuarioFalsoResponse.usuarioId);
+
+        }
+    }
+}
